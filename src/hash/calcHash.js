2040fff86ab0047ceffa57f715b5950fff89416a
Aliaksandra Rahachova <klaustrafobiyakl@gmail.com>
2024-10-05 22:18:55 +0200

    feat: add solution for the hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const stream = createReadStream(
+    `${__dirname}/files/fileToCalculateHashFor.txt`,
+    {
+      encoding: "utf-8",
+    }
+  );
+
+  const hash = createHash("sha256");
+
+  stream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on("end", () => {
+    console.log("Hash for file fileToCalculateHashFor.txt is ready:\n");
+    console.log(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
