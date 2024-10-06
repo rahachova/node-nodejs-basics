24bfb857c346f54216be2acf79d4e72c73541ec0
Aliaksandra Rahachova <klaustrafobiyakl@gmail.com>
2024-10-06 16:57:50 +0200

    feat: add solutions for the zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const readableStream = createReadStream(
+    `${__dirname}/files/fileToCompress.txt`
+  );
+  const writebleStream = createWriteStream(`${__dirname}/files/archive.gz`);
+  const gzipStream = createGzip();
+
+  readableStream.pipe(gzipStream).pipe(writebleStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createUnzip } from "node:zlib";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const readableStream = createReadStream(`${__dirname}/files/archive.gz`);
+  const writebleStream = createWriteStream(
+    `${__dirname}/files/fileToCompress.txt`
+  );
+  const unzipStream = createUnzip();
+
+  readableStream.pipe(unzipStream).pipe(writebleStream);
 };
 
 await decompress();
