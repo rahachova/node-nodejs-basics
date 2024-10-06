401672d431e1a8cca039a992e5d581dd30b9a1b7
Aliaksandra Rahachova <klaustrafobiyakl@gmail.com>
2024-10-06 20:54:21 +0200

    feat: add solution for the worker threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import { Worker } from "node:worker_threads";
+import { cpus } from "os";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createWorker = (number) => {
+  return new Promise((resolve) => {
+    const worker = new Worker(`${__dirname}/worker.js`, {
+      workerData: number,
+    });
+
+    worker.once("message", (message) => {
+      resolve({ status: "resolved", data: message });
+    });
+    worker.once("error", () => {
+      resolve({ status: "error", data: null });
+    });
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerPromises = [];
+
+  for (let index = 0; index < cpus().length; index++) {
+    workerPromises.push(createWorker(10 + index));
+  }
+
+  const workerResult = await Promise.all(workerPromises);
+  console.log(workerResult);
 };
 
 await performCalculations();
